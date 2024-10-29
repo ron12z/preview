1787a02643d7c47d066c78af8c491e3183b82896
ron12z <aarontalua14@gmail.com>
2024-10-29 21:09:30 +0800

    checkpoint -- escalation crazy

--- src/scripts/escalation.js
+++ src/scripts/escalation.js
@@ -1,44 +1,31 @@
 import { getRestrictions } from "./restriction";
 
-const escalations = {};
-
-function addRestriction(key, value) {
-	if (!(key in escalations)) {
-		escalations.key = value;
-	}
-}
-
-function removeRestriction(key) {
-	if (key in escalations) {
-		delete escalations.key;
-	}
-}
-
-function getEscalation() {
-	let result = [];
-
-	Object.values(escalations).forEach((value) => {
-		result.push(value);
-	});
-
-	if (result.length > 1) {
-		result = result.slice(0, -1).join(", ") + ", and " + result.slice(-1);
-	} else if (result.length === 1) {
-		result = result[0];
-	} else if (result.length === 0) {
-		result = "No restriction selected";
-	}
-
-	return result;
-}
-
 function updateResult() {
+	// Update escalation result
+	const escalations = {};
+
 	const allRestrictions = getRestrictions();
-
-	const escalationResult = document.querySelector("#escalation-result");
-	escalationResult.textContent = getEscalation();
-
 	const allChecked = document.querySelectorAll("option-plain.checked");
+
+	function addRestriction(restriction) {
+		if (!(restriction.code in escalations)) {
+			escalations.code = restriction.escalation;
+		}
+	}
+
+	function getEscalation() {
+		let result = [];
+
+		if (result.length > 1) {
+			result = result.slice(0, -1).join(", ") + ", and " + result.slice(-1);
+		} else if (result.length === 1) {
+			result = result[0];
+		} else if (result.length === 0) {
+			result = "No restriction selected";
+		}
+
+		return result;
+	}
 
 	allChecked.forEach((option) => {
 		const id = option.getAttribute("id");
@@ -46,12 +33,10 @@ function updateResult() {
 		addRestriction(restriction);
 	});
 
-	console.log(getEscalation());
+	const escalationResult = document.querySelector("#escalation-result");
+	escalationResult.textContent = getEscalation();
 }
 
 export default {
-	addRestriction,
-	removeRestriction,
-	getEscalation,
 	updateResult,
 };
