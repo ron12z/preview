8dd5e83ba7d7f800f2bd691c6a3bf5454f18abcd
ron12z <aarontalua14@gmail.com>
2024-10-30 19:34:40 +0800

    surrender and retry

--- src/scripts/restriction.ts
+++ src/scripts/restriction.ts
@@ -2,16 +2,18 @@ interface Options {
 	code?: string;
 	description?: string;
 	escalation?: string;
-	options?: string[] | null;
+	optionDesc?: string | null;
+	options?: Object | null;
 	numOfInputs?: number | null;
 	addMoreButton?: boolean;
 }
 
-class Restriction {
+export default class Restriction {
 	code: string;
 	description: string;
 	escalation: string;
-	options: string[] | null;
+	optionDesc?: string | null;
+	options: Object | null;
 	numOfInputs: number | null;
 	addMoreButton: boolean;
 
@@ -19,6 +21,7 @@ class Restriction {
 		code = "sa1",
 		description = "Sample Restriction",
 		escalation = "Suspicious Activity 1",
+		optionDesc = null,
 		options = null,
 		numOfInputs = null,
 		addMoreButton = false,
@@ -26,6 +29,7 @@ class Restriction {
 		this.code = code;
 		this.description = description;
 		this.escalation = escalation;
+		this.optionDesc = optionDesc;
 		this.options = options;
 		this.numOfInputs = numOfInputs;
 		this.addMoreButton = addMoreButton;
@@ -41,20 +45,35 @@ class Restriction {
 	}
 }
 
-const Restrictions: { [key: string]: Restriction } = {};
+export const Restrictions: { [key: string]: Restriction } = {};
 
 export const getRestrictions = () => {
 	return Restrictions;
 };
 
-export default Restriction;
-
 // Creating restrictions
 new Restriction({
 	code: "threshold",
-	description: "Threshold",
-	escalation: "Threshold escal",
-	options: null,
+	description: "More than threshold",
+	escalation: "More than thresold escalation",
+	optionDesc: "Account Status",
+	options: {
+		option1: ["VIP", "wd greater than 25k"],
+		option2: ["Not VIP", "wd greater than 24.999.99"],
+	},
+	numOfInputs: null,
+	addMoreButton: false,
+});
+
+new Restriction({
+	code: "accountStatus",
+	description: "Account is Suspended/Closed",
+	escalation: "account is suspended or closed based on selection",
+	optionDesc: "Account Status",
+	options: {
+		option1: ["Closed", "account status is closed"],
+		option2: ["Suspended", "account is suspended"],
+	},
 	numOfInputs: null,
 	addMoreButton: false,
 });
@@ -72,69 +91,6 @@ new Restriction({
 	code: "sa2",
 	description: "Suspicious Activity 2",
 	escalation: "Suspicious Activity 2 escal",
-	options: null,
-	numOfInputs: null,
-	addMoreButton: false,
-});
-
-new Restriction({
-	code: "sa3",
-	description: "Suspicious Activity 3",
-	escalation: "Suspicious Activity 3 escal",
-	options: null,
-	numOfInputs: null,
-	addMoreButton: false,
-});
-
-new Restriction({
-	code: "sa3",
-	description: "Suspicious Activity 3",
-	escalation: "Suspicious Activity 3 escal",
-	options: null,
-	numOfInputs: null,
-	addMoreButton: false,
-});
-
-new Restriction({
-	code: "sa3",
-	description: "Suspicious Activity 3",
-	escalation: "Suspicious Activity 3 escal",
-	options: null,
-	numOfInputs: null,
-	addMoreButton: false,
-});
-
-new Restriction({
-	code: "sa3",
-	description: "Suspicious Activity 3",
-	escalation: "Suspicious Activity 3 escal",
-	options: null,
-	numOfInputs: null,
-	addMoreButton: false,
-});
-
-new Restriction({
-	code: "sa3",
-	description: "Suspicious Activity 3",
-	escalation: "Suspicious Activity 3 escal",
-	options: null,
-	numOfInputs: null,
-	addMoreButton: false,
-});
-
-new Restriction({
-	code: "sa3",
-	description: "Suspicious Activity 3",
-	escalation: "Suspicious Activity 3 escal",
-	options: null,
-	numOfInputs: null,
-	addMoreButton: false,
-});
-
-new Restriction({
-	code: "sa3",
-	description: "Suspicious Activity 3",
-	escalation: "Suspicious Activity 3 escal",
 	options: null,
 	numOfInputs: null,
 	addMoreButton: false,
