df4c604cce5ffe1821739b883f9c4889afc3d2a9
ron12z <aarontalua14@gmail.com>
2024-11-14 14:20:53 +0800

    Only need popup

--- src/scripts/copyEscalation.js
+++ src/scripts/copyEscalation.js
@@ -37,6 +37,7 @@ function CtoCopy(event) {
 	}
 }
 
+const escalation = document.querySelector(".escalation");
 const result = document.querySelector("#escalation-result");
 const header = document.querySelector("header");
 const headerBG = window.getComputedStyle(header).backgroundColor;
@@ -45,14 +46,16 @@ export function setEscalationHandler() {
 	const activeEscalations = getEscalations();
 
 	if (Object.keys(activeEscalations).length != 0) {
-		result.addEventListener("click", handleEscalationClick);
+		escalation.addEventListener("click", handleEscalationClick);
+		result.classList.remove("no-content");
 		document.addEventListener("keydown", CtoCopy);
 		header.style.background = "rgb(160, 60, 60)";
-		result.style.cursor = "pointer";
+		escalation.style.cursor = "pointer";
 	} else {
-		result.removeEventListener("click", handleEscalationClick);
+		escalation.removeEventListener("click", handleEscalationClick);
+		result.classList.add("no-content");
 		document.removeEventListener("keydown", CtoCopy);
 		header.style.background = `${headerBG}`;
-		result.style.cursor = "not-allowed";
+		escalation.style.cursor = "not-allowed";
 	}
 }
